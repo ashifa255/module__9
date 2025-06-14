610ca23a9e5da2b17e3a1661905254ca3ec79cdd
ashifa255 <astammannahifa0330@gmail.com>
2025-06-14 12:35:35 +0600

    js

---
+++ script.js
@@ -0,0 +1,23 @@
+const gallery = document.getElementById('gallery');
+fetch('https://picsum.photos/v2/list?page=2&limit=12')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(photo => {
+      const imageCard = document.createElement('div');
+      imageCard.classList.add('gallery-item');
+
+      imageCard.innerHTML = `
+        <img src="${photo.download_url}" alt="${photo.author}">
+        <div class="info" style="padding: 10px;">
+          <p>Author: ${photo.author}</p>
+        </div>
+      `;
+
+      gallery.appendChild(imageCard);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching photos:', error);
+    gallery.innerHTML = <p style="color: red; text-align:center;">Failed to load images. Try again later.</p>;
+  });
+picsum.photos
